708ebc8420fe824acdd803415d25cd961a36827a
RamyaOduri <ramya.oduri@narayanagroup.com>
2023-05-01 11:21:27 +0530

    animations

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyGridScreen(),
+    );
+  }
+}
+
+class MyGridScreen extends StatefulWidget {
+  MyGridScreen({super.key});
+
+  @override
+  _MyGridScreenState createState() => _MyGridScreenState();
+}
+
+class _MyGridScreenState extends State<MyGridScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter GridView Demo"),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+            child: GridView.extent(
+          primary: false,
+          padding: const EdgeInsets.all(16),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          maxCrossAxisExtent: 200.0,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text('First', style: TextStyle(fontSize: 20)),
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text('Second', style: TextStyle(fontSize: 20)),
+              color: Colors.blue,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Third', style: TextStyle(fontSize: 20)),
+              color: Colors.blue,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Four', style: TextStyle(fontSize: 20)),
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Fifth', style: TextStyle(fontSize: 20)),
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Six', style: TextStyle(fontSize: 20)),
+              color: Colors.blue,
+            ),
+          ],
+        )),
+      ),
+      // builder: (context,child) => Padding(padding: EdgeInsets.only(top:100 - _animationController.value * 100),child: child,));
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+              parent: _animationController, curve: Curves.easeInOut),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
